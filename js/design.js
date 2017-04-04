fdb9a918fecb5e53087df68343b9c07385b8dfd6
jhautot <jhautot@gmail.com>
2017-04-04 18:50:03 +0200

    margin right footer dynamique

---
+++ js/design.js
@@ -0,0 +1,31 @@
+
+
+function update_design_footer()
+{
+    var widthContact = document.getElementById('contact').offsetWidth;
+    var widthArticlesRecents = document.getElementById('articlesRecents').offsetWidth;
+    var widthSitesDeReferences = document.getElementById('sitesDeReferences').offsetWidth;
+    
+    var widthCorps = $('#corps').width(); // largeur sans padding
+    
+    var leftSpace = widthCorps - widthSitesDeReferences - widthArticlesRecents - widthContact;
+    
+    if (leftSpace < 0) {
+        console.log("Il n'y a pas assez d'espace pour afficher tous les éléments sur une même ligne"); // to remove.
+    }
+    
+    var contactMarginRight = (2*leftSpace)/3;
+    var widthArticlesRecents = leftSpace - contactMarginRight;
+    document.getElementById("contact").style.marginRight = contactMarginRight + "px";
+    document.getElementById("articlesRecents").style.marginRight = widthArticlesRecents + "px";    
+}
+
+function update_design()
+{
+    update_design_footer();
+}
+
+
+window.onresize = function() {
+    update_design();
+};

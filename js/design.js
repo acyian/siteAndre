954c55e46657bb6c6f0417994ad5d86017ea7f9c
jhautot <jhautot@gmail.com>
2017-08-18 23:29:12 +0200

    design image javascript

--- js/design.js
+++ js/design.js
@@ -24,8 +24,80 @@ function update_design_footer(){
     }  
 }
 
+function update_design_images(){
+    var widthCorps = $("#corps").width();
+    var widthContenuPrincipal = $("#contenuPrincipal").width();
+    $('.conteneurImageSeule').each(function(){
+        
+        // Dabbord remettre les margin originale pour que la div parente aie la bonne taille.
+        if ($(this).hasClass("imgFloatRight")) {
+            $(this).children().first().css("margin-left", 30 + "px");
+            $(this).children().first().css("margin-right", 0 + "px");
+        }
+        else {
+            $(this).children().first().css("margin-left", 0 + "px");
+            $(this).children().first().css("margin-right", 30 + "px");
+        }
+            
+        var widthElementCourant = $(this).width();
+        var nbImages = $(this).find('img').length;
+                
+        if (widthElementCourant/widthCorps > 0.7 && (nbImages == 1)) {
+            $(this).find('img').css("margin-left", "0px");
+            $(this).find('img').css("margin-right", "0px");
+            $(this).find('img').css("max-width", widthCorps + "px");
+            $(this).children().first().css("display", "block");
+            
+            //var widthDivImg = $(this).children().first().width();
+            var widthDivImg = $(this).find('img').width();
+            $(this).children().first().css("margin-left", (widthContenuPrincipal-widthDivImg)/2 + "px");
+            $(this).children().first().css("margin-right", (widthContenuPrincipal-widthDivImg)/2 + "px");
+        }
+        else {
+            $(this).find('img').css("margin-left", "0px");
+            $(this).find('img').css("margin-right", "30px");
+            $(this).find('img').css("max-width", widthCorps + 30 + "px");
+            $(this).children().first().css("display", "inline-block");
+            
+            if ($(this).hasClass("imgFloatRight")) {
+                $(this).children().first().css("margin-left", 30 + "px");
+                $(this).children().first().css("margin-right", 0 + "px");
+            }
+            else {
+                $(this).children().first().css("margin-left", 0 + "px");
+                $(this).children().first().css("margin-right", 30 + "px");
+            }
+        }      
+        
+    });
+    
+    $('.conteneurImages').each(function(){
+        var widthElementCourant = $(this).width();
+        var nbImages = $(this).find('img').length;
+        
+        if (widthElementCourant/widthCorps > 0.7 && nbImages == 1) {
+            $(this).find('img').css("margin-left", "0px");
+            $(this).find('img').css("margin-right", "0px");
+            $(this).find('img').css("max-width", widthCorps + "px");
+            $(this).children().first().css("display", "block");
+        }
+        else {
+            $(this).find('img').css("margin-left", "20px");
+            $(this).find('img').css("margin-right", "20px");
+            $(this).find('img').css("max-width", widthCorps - 40 + "px");
+            $(this).children().first().css("display", "inline-block");
+        }      
+        
+    });
+}
+
 function update_design(){
     update_design_footer();
+}
+
+function update_design_when_header_loaded(){
+    design_menus_responsive();
+    update_design_images();
 }
 
 function changements_generaux_onresize() {
@@ -33,7 +105,7 @@ function changements_generaux_onresize() {
     open_or_close_menu_gauche_onresize();
     open_or_close_menu_droite_onresize();
     design_corps_responsive();
-    design_menus_responsive();
+    update_design_when_header_loaded();
 }
 
 window.onresize = function() {
